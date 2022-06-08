a984a749e61120f2cdb6f31128b6467488735e77
Marcin Copik <mcopik@gmail.com>
2022-06-08 20:44:45 +0200

    [tools] Handle multiple regions

--- tools/postprocessing.py
+++ tools/postprocessing.py
@@ -19,7 +19,7 @@ def process(filename: str):
     start = 0
     stop = None
     line_count = 0
-    data = []
+    data = {}
 
     sum_read = 0
 
@@ -31,47 +31,59 @@ def process(filename: str):
 
         name = files[i]
         iteration = int(name.split('.')[-1])
+        region = name.split('.')[-2]
         in_file = open(name, 'r')
         line = in_file.readline()
         df = pd.read_table(in_file, sep=" ", names=["address", "size", "read", "write"], converters={'address': partial(int, base=16)})
-        data.append((iteration, df)) #.groupby(["address", "op"]).sum().reset_index()))
 
-    print(f"Read {len(data)} files")
+        if region not in data:
+            data[region] = []
+        data[region].append((iteration, df)) #.groupby(["address", "op"]).sum().reset_index()))
 
-    data.sort(key=lambda x: x[0])
 
+    total_files = 0
+    for region, region_data in data.items():
+        region_data.sort(key=lambda x: x[0])
+        total_files += len(region_data)
+
+    print(f"Read {total_files} files for {len(data)} regions")
     print("Parsing memory data")
-    iteration = 0
-    read_addresses = set()
-    write_addresses = set()
     result = []
 
-    for i in tqdm(range(len(data))):
+    with tqdm(total=total_files) as pb:
 
-        df = data[i][1]
-        iteration += 1
+        for region, region_data in data.items():
 
-        read = set()
-        for val in df.loc[df['read'] > 0].itertuples():
-            #[['address', 'size']]:
-            for j in range(val.size):
-                read.add(val.address + j)
+            read_addresses = set()
+            write_addresses = set()
 
-        old_read_addresses_len = len(read_addresses)
-        read_addresses.update(read)
+            for data in region_data:
+                df = data[1]
+                iteration = data[0]
 
-        write = set()
-        for val in df.loc[df['write'] > 0].itertuples():
-            for j in range(val.size):
-                write.add(val.address + j)
+                read = set()
+                for val in df.loc[df['read'] > 0].itertuples():
+                    #[['address', 'size']]:
+                    for j in range(val.size):
+                        read.add(val.address + j)
 
-        old_write_addresses_len = len(write_addresses)
-        write_addresses.update(write)
+                old_read_addresses_len = len(read_addresses)
+                read_addresses.update(read)
 
-        result.append([i, len(read), (len(read_addresses) - old_read_addresses_len), len(write), (len(write_addresses) - old_write_addresses_len)])
+                write = set()
+                for val in df.loc[df['write'] > 0].itertuples():
+                    for j in range(val.size):
+                        write.add(val.address + j)
+
+                old_write_addresses_len = len(write_addresses)
+                write_addresses.update(write)
+
+                result.append([region, iteration, len(read), (len(read_addresses) - old_read_addresses_len), len(write), (len(write_addresses) - old_write_addresses_len)])
+
+                pb.update(1)
 
 
-    df = pd.DataFrame(data=result, columns=["iteration", "read_bytes", "new_read_bytes", "write_bytes", "new_write_bytes"])
+    df = pd.DataFrame(data=result, columns=["region", "iteration", "read_bytes", "new_read_bytes", "write_bytes", "new_write_bytes"])
     return df
 
 if __name__ == "__main__":
